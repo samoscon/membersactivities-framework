f57cfca294eae23d7d548d159f081e8084d87938
samoscon <53895420+samoscon@users.noreply.github.com>
2025-10-29 15:47:20 +0100

    Update ActivityCommand.php

--- commands/viewrender/user/ActivityCommand.php
+++ commands/viewrender/user/ActivityCommand.php
@@ -19,10 +19,16 @@ class ActivityCommand extends \controllers\Command {
     #[\Override]
     public function doExecute(\registry\Request $request): int {
         /** Variables */
+
         $propertiesMember = array();
         $propertiesSubscription = array();
         $propertiesPayment = array();
-        $nameIsEmpty = $emailIsEmpty = $quantityIsEmpty = false;
+        $emailIsEmpty = $nameIsEmpty = false;
+        $quantityIsEmpty = false;
+        
+        if(_MINLEVELTOLOGIN !== 'A') {
+            $responses['member'] = $member = \sessions\User::getInstance();
+        }
         
         $id = filter_var($request->get('id'), FILTER_VALIDATE_INT);
         if(!$id) {
@@ -58,10 +64,12 @@ class ActivityCommand extends \controllers\Command {
 
         /** Check that the page was requested from itself via the POST method. */
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $propertiesMember['name'] = $name = filter_var($request->get('name'), FILTER_UNSAFE_RAW);
-            $responses['nameIsEmpty'] = $nameIsEmpty = $name ? false : true;
-            $propertiesMember['email'] = $email = filter_var($request->get("email"), FILTER_VALIDATE_EMAIL);
-            $responses['emailIsEmpty'] = $emailIsEmpty = $email ? false : true;
+            if(_MINLEVELTOLOGIN === 'A') {
+                $propertiesMember['name'] = $name = filter_var($request->get('name'), FILTER_UNSAFE_RAW);
+                $responses['nameIsEmpty'] = $nameIsEmpty = $name ? false : true;
+                $propertiesMember['email'] = $email = filter_var($request->get("email"), FILTER_VALIDATE_EMAIL);
+                $responses['emailIsEmpty'] = $emailIsEmpty = $email ? false : true;
+            }
             $total = 0;
             foreach ($costitems as $costitem) {
                 $total += (filter_var($request->get($costitem->getId()), FILTER_VALIDATE_INT) * $costitem->price);
@@ -71,51 +79,53 @@ class ActivityCommand extends \controllers\Command {
             if (!$nameIsEmpty && !$emailIsEmpty) {
                 $validator = $this->reg->getSubscriptionValidator('User');
                 
-                $memberid = $this->reg->getLoginManager()->validateUsername($email);   
-                if(! $memberid) {
-                    $propertiesMember['active'] = 1;
-                    $propertiesMember['subscriptionuntil'] = '2099-12-31';
-                    \model\Member::insert($propertiesMember); //Member is not found and should be created
-                    $memberid = $this->reg->getLoginManager()->validateUsername($email);
-                    $member = \model\Member::find($memberid);
-                }
-                try {
-                    $member = \model\Member::find($memberid);
-                } catch (\Exception $exc) {
-                    $request->addFeedback($exc->getMessage());
-                    return self::CMD_ERROR;
-                }
-                $member->update($propertiesMember);
-                $member->active = 1;
-                $member->subscriptionuntil = '2099-12-31';
-                $user = $this->reg->getLoginManager()->login($memberid);
-
-                $propertiesSubscription['member_id'] = $propertiesPayment['member_id'] = $memberid;
-                $propertiesPayment['amount'] = $total;
-                $propertiesPayment['status'] = $total ? 'open' : 'paid';
-                $orderid = \model\Payment::insert($propertiesPayment)->getId();
-
-                foreach ($costitems as $costitem) {
-                    $propertiesSubscription['quantity'] = $quantity = filter_var($request->get($costitem->getId()), FILTER_VALIDATE_INT);
-                    $propertiesSubscription['costitem_id'] = $costitem->getId();
-                    $propertiesSubscription['payment_id'] = $orderid;
-                        
-                    if ($quantity) {
-                        $check = $validator->subscribe($member, $costitem, $propertiesSubscription);
-                        if($check['errorcode']) {
-                            $request->addFeedback("Uw inschrijving is niet doorgegaan: " . $check['description']);
-                            return self::CMD_ERROR;
-                        }                                       
+                if(_MINLEVELTOLOGIN === 'A') {               
+                    $memberid = $this->reg->getLoginManager()->validateUsername($email);   
+                    if(! $memberid) {
+                        $propertiesMember['active'] = 1;
+                        $propertiesMember['subscriptionuntil'] = '2099-12-31';
+                        \model\Member::insert($propertiesMember); //Member is not found and should be created
+                        $memberid = $this->reg->getLoginManager()->validateUsername($email);
+                        $member = \model\Member::find($memberid);
                     }
+                    try {
+                        $member = \model\Member::find($memberid);
+                    } catch (\Exception $exc) {
+                        $request->addFeedback($exc->getMessage());
+                        return self::CMD_ERROR;
+                    }
+                    $member->update($propertiesMember);
+                    $member->active = 1;
+                    $member->subscriptionuntil = '2099-12-31';
                 }
-                           
-                if ($total > 0) {
-                    $request->set('forwardqueryparams', array('id' => $orderid*171963, 'chk' => "BM*171963"));
-                    return self::CMD_OK;
-                } else {
-                    $request->set('forwardqueryparams', array('id' => $orderid*171963, 'chk' => "BM*171963"));
-                    return self::CMD_ADMIN;
+            }
+
+
+            $propertiesSubscription['member_id'] = $propertiesPayment['member_id'] = $member->getId();
+            $propertiesPayment['amount'] = $total;
+            $propertiesPayment['status'] = $total ? 'open' : 'paid';
+            $orderid = \model\Payment::insert($propertiesPayment)->getId();
+
+            foreach ($costitems as $costitem) {
+                $propertiesSubscription['quantity'] = $quantity = filter_var($request->get($costitem->getId()), FILTER_VALIDATE_INT);
+                $propertiesSubscription['costitem_id'] = $costitem->getId();
+                $propertiesSubscription['payment_id'] = $orderid;
+
+                if ($quantity) {
+                    $check = $validator->subscribe($member, $costitem, $propertiesSubscription);
+                    if($check['errorcode']) {
+                        $request->addFeedback("Uw inschrijving is niet doorgegaan: " . $check['description']);
+                        return self::CMD_ERROR;
+                    }                                       
                 }
+            }
+
+            if ($total > 0) {
+                $request->set('forwardqueryparams', array('id' => $orderid*171963, 'chk' => "BM*171963"));
+                return self::CMD_OK;
+            } else {
+                $request->set('forwardqueryparams', array('id' => $orderid*171963, 'chk' => "BM*171963"));
+                return self::CMD_ADMIN;
             }
         } 
             
