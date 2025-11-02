3df119bcc232476c2fd1ae3be3a9845e23087188
samoscon <53895420+samoscon@users.noreply.github.com>
2025-11-02 12:31:08 +0100

    Update paymentConfirmView.php

--- example/MVCFramework/views/user/paymentConfirmView.php
+++ example/MVCFramework/views/user/paymentConfirmView.php
@@ -1,3 +1,7 @@
+<?php
+$payment = $request->get('payment');
+$activity = $request->get('activity');
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,15 +11,17 @@
     <body>
         <div class="container-fluid p-3 bg-white text-black-50">
             <div class="d-flex w-100 justify-content-between">
-                 <img src="assets/bm-WHITE.jpg" alt="Brussels Muzieque" width="35%"> 
+                 <img src=<?=_LOGO?> alt="Logo ontbreekt" width="35%"> 
                 <p class="text-center"><b>Feedback</b></p>
                 <p> </p>
             </div>
         </div>
         <div class="container text-primary m-5">
             <div class="row">
-<!--                <p class="text-primary"><?= $request->getFeedbackString(); ?></p>-->
-                <?= $request->getFeedbackString(); ?>
+                <p>We have received your payment. A mail with your tickets has also been sent to your mailaddress <b>
+                        <?=$payment->member->email?></b></p>
+                <p><?=$payment->paymenttypeimplementation->getOrderedTicketsText($payment)?></p>
+                <p><?=$payment->paymenttypeimplementation->getDownloadText($payment, $activity)?></p>
                 <p><a href=<?=_HOMEPAGE?> class="btn btn-primary btn-block m-4" role="button">Back to website</a></p>
             </div>
         </div>
