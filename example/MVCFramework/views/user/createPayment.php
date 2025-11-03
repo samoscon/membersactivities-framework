501425e86508af643eedb22ee586a54dbf8b2473
samoscon <53895420+samoscon@users.noreply.github.com>
2025-11-03 13:39:22 +0100

    Update createPayment.php

--- example/MVCFramework/views/user/createPayment.php
+++ example/MVCFramework/views/user/createPayment.php
@@ -12,26 +12,26 @@ $payment = $request->get('payment');
         <div class="container-fluid p-3 bg-white text-black-50">
             <div class="d-flex w-100 justify-content-between">
                  <img src=<?=_LOGO?> alt="Logo ontbreekt" width="35%"> 
-                <p class="text-center"><b>Betalingswijze</b></p>
+                <p class="text-center"><b>Payment method</b></p>
                 <p> </p>
             </div>
         </div>
         <div class="container mt-3">
-            <h2 style="text-align:center">Uw inschrijving is aangemaakt.</h2>
+            <h2 style="text-align:center">Your order is created.</h2>
             <form autocomplete="off" method="POST">
                 <div class="card">
                     <div class="row m-5"
-                        <p><b>Welke betalingswijze verkies je:</b></p>
-  			<input type="radio" id="online" name="paymentmethod" value="online" data-role="none"  onclick="toggleWireTransfer()" checked> Ik betaal online<br>
-  			<input type="radio" id="wire" name="paymentmethod" value="wire"  onclick="toggleWireTransfer()" data-role="none"> Ik schrijf het bedrag rechtstreek over<br>
+                        <p><b>Payment method:</b></p>
+  			<input type="radio" id="online" name="paymentmethod" value="online" data-role="none"  onclick="toggleWireTransfer()" checked> I pay online<br>
+  			<input type="radio" id="wire" name="paymentmethod" value="wire"  onclick="toggleWireTransfer()" data-role="none"> I wire the amount to the account of <?=_MAILFROMNAME?><br>
   			<div class="text-primary" id="wiretransfer" style="display:none;">
-  			    Om over te schrijven, gebruik de volgende gegevens:
+  			    For the wire transfer, please use following data:
   			    <ul>
   				<li><b>IBAN:</b> BExx xxxx xxxx xxxx</li>
   				<li><b>BIC:</b> GEBABEBB</li>
-  				<li><b>Naam:</b> <?=_MAILFROMNAME?></li>
-  				<li><b>Bedrag:</b> <?=$payment->amount?> EUR</li>
-  				<li><b>Referentie:</b> <?=APP?> orderid <?=$payment->getId()?></li>
+  				<li><b>Name:</b> <?=_MAILFROMNAME?></li>
+  				<li><b>Amount:</b> <?=$payment->amount?> EUR</li>
+  				<li><b>Reference:</b> <?=APP?> orderid <?=$payment->getId()?></li>
   			    </ul>
   			</div>
                     </div>
