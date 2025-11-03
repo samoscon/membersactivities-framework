e21d7056d13608ece43697c9118ba06e3afc654e
samoscon <53895420+samoscon@users.noreply.github.com>
2025-11-03 11:24:22 +0100

    Add files via upload

---
+++ commands/viewrender/user/PaymentConfirmationCommand.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Specialization of a Command
+ *
+ * @package commands\user
+ * @version 4.0
+ * @copyright (c) 2024, Dirk Van Meirvenne
+ * @author Dirk Van Meirvenne <van.meirvenne.dirk at gmail.com>
+ */
+namespace commands\viewrender\user;
+
+/**
+ * Specialization of a Command
+ *
+ * @author Dirk Van Meirvenne <van.meirvenne.dirk at gmail.com>
+ */
+class PaymentConfirmationCommand extends \controllers\Command {
+    
+    /**
+     * Specialization of the execute method of Command
+     * 
+     * @param \registry\Request $request
+     * @return int
+     */
+    #[\Override]
+    public function doExecute(\registry\Request $request): int {        
+        $id = filter_var($request->get('order_id'), FILTER_VALIDATE_INT)/171963;
+
+        try {
+            $payment = \model\Payment::find($id); 
+        } catch (\Exception $exc) {
+            $request->addFeedback($request->get('feedbackPaymentNotFound'));
+            return self::CMD_ERROR;
+        }
+        
+        if (!$payment->isPaid()) {
+            $request->addFeedback($request->get('feedbackPaymentNotPaid'));
+            return self::CMD_ERROR;
+        }
+        
+        $activity = $payment->paymenttypeimplementation->getSubscription($payment)->costitem->activity;
+
+        $responses['payment'] = $payment;
+        $responses['activity'] = $activity;
+        
+        $this->addResponses($request, $responses);
+        return self::CMD_DEFAULT;
+    }
+    
+    /**
+     * Specialization of getLevelOfLoginRequired
+     */
+    #[\Override]
+    protected function getLevelOfLoginRequired(): void {
+        $this->setLoginLevel(new \sessions\NoLoginRequired());
+    }
+
+}
